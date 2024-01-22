d0edbc0244d3a268b26c42a886cc8484eab0c942
Barun Nafak <bnafak@gmail.com>
2024-01-22 22:00:13 +0600

    added money on Dad component

--- src/Dad.jsx
+++ src/Dad.jsx
@@ -1,11 +1,17 @@
+import { useContext, useState } from "react";
 import Brother from "./Brother";
 import Myself from "./Myself";
 import Sister from "./Sister";
+import { MoneyContext } from "./Grandpa";
 
 const Dad = ({asset}) => {
+  
+  const [money, setMoney] = useContext(MoneyContext);
+ 
   return (
     <div>
       <h2>Dad</h2>
+      <h3>{money}</h3>
       <section className="flex">
         <Myself asset="asset"></Myself>
         <Brother></Brother>
